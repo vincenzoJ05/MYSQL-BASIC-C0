57c0471d6f69d3e4079a5483eeca8e26af7a8b5e
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-06 13:29:43 +0100

    opdracht02-SELECT af

--- 02-Select/Opdracht02-SELECT/antwoorden.sql
+++ 02-Select/Opdracht02-SELECT/antwoorden.sql
@@ -1,7 +1,11 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-
+SELECT * FROM `jaar2015`
+SELECT score FROM `jaar2016`
+SELECT region FROM `jaar2015`
+SELECT region , score FROM `jaar2015`
+SELECT score , rank FROM `jaar2016`
 -- Opdracht 3
 
 -- Opdracht 4
