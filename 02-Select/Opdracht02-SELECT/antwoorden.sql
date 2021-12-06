af1081ec50e810435d26c39627bd5fc522544188
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-12-06 18:10:58 +0100

    opdracht04 af

--- 02-Select/Opdracht02-SELECT/antwoorden.sql
+++ 02-Select/Opdracht02-SELECT/antwoorden.sql
@@ -19,7 +19,22 @@ SELECT country , region, rank FROM `jaar2016` WHERE rank <= 5
 SELECT score FROM `jaar2016` WHERE country= "ireland"
 SELECT country , region FROM `jaar2015` WHERE region="Latin America and Caribbean"
 -- Opdracht 4
-
+SELECT * FROM `players` WHERE nationality = 'spain' AND club = 'chelsea'
+SELECT * FROM `players` WHERE age = 17 AND nationality = 'spain' AND wage = 15000
+SELECT * FROM `players` WHERE age >=20 AND club = 'Liverpool'
+SELECT * FROM `players` WHERE nationality = 'Netherlands' AND club = 'Ajax'
+SELECT * FROM `players` WHERE club = 'ajax' AND nationality != 'Netherlands'h
+/* vanaf hier opdracht 6*/
+SELECT name , age, FROM `players` WHERE club = 'AZ Alkmaar'
+SELECT name , age, club FROM `players` WHERE club = 'AZ Alkmaar'
+SELECT name , wage FROM `players` WHERE nationality = 'Brazil' AND club = 'Manchester City'
+SELECT name FROM `players` WHERE age = 30 AND wage <= 10000
+SELECT name , age FROM `players` WHERE nationality = 'spain' OR nationality = 'portugal'
+SELECT name , age, club FROM `players` WHERE nationality = 'portugal' OR club = 'Chelsea'
+SELECT name , club FROM `players` WHERE age >= 40 OR wage >= 10000
+SELECT * FROM `players` WHERE nationality = 'netherlands' OR club = 'Ajax' AND club = 'FC Utrecht'
+SELECT * FROM `players` WHERE nationality = 'England' AND age >= 20 AND wage >= 100000 
+SELECT name , age , nationality FROM `players` WHERE nationality = 'Argentine' OR nationality = 'Brazil' AND age>= 25
 -- Opdracht 5
 
 -- Opdracht 6
