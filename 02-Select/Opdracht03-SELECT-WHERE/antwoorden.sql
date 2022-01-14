2b897ade72582bf83b40e2f0caeeefff97f9814d
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2022-01-14 21:19:19 +0100

    verbeterd

--- 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
+++ 02-Select/Opdracht03-SELECT-WHERE/antwoorden.sql
@@ -1,19 +1,20 @@
 -- Opdracht 1 
-
+SELECT country FROM `jaar2015` WHERE rank=7;
 -- Opdracht 2 
-
+SELECT rank FROM `jaar2016` WHERE country= "Netherlands";
 -- Opdracht 3
-
+SELECT score FROM `jaar2015` WHERE country= "Netherlands";   
 -- Opdracht 4
-
+SELECT score FROM `jaar2016` WHERE country= "Netherlands";
 -- Opdracht 5
-
+SELECT country , region FROM `jaar2015` WHERE region="North America";
 -- Opdracht 6 
-
+SELECT country , rank FROM `jaar2016` WHERE rank=25
 -- Opdracht 7 
-
+SELECT country , score FROM `jaar2016` WHERE score > 7087
 -- Opdracht 8
-
+SELECT country , region, rank FROM `jaar2016` WHERE rank <= 5
 -- Opdracht 9
-
+SELECT score FROM `jaar2015` WHERE country= "ireland"
 -- Opdracht 10
+SELECT country FROM `jaar2015` WHERE region="Latin America and Caribbean"
